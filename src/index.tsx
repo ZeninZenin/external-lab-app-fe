db4d62841f9683060fe19a143588280be4d00971
Sergey Zenin <sergey_zenin@epam.com>
2022-02-22 23:51:00 +0300

    add "edit username" feature

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,13 @@ import { App } from './app/App.component';
 import reportWebVitals from './reportWebVitals';
 import 'antd/dist/antd.css';
 import { AppContextProvider } from './context';
-localStorage.setItem(
-  'token',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dpbiI6IlplbmluWmVuaW4iLCJpc0xvY2tlZCI6ZmFsc2UsInJvbGVzIjpbImFkbWluIl0sImZpcnN0TmFtZSI6ItCh0LXRgNCz0LXQuSIsImdpdGh1Yk5hbWUiOiJTZXJnZXkgWmVuaW4iLCJsYXN0TmFtZSI6ItCX0LXQvdC40L0iLCJpYXQiOjE2NDU1MTI5NTMsImV4cCI6MTY0NTU5OTM1M30.fhknNiL-2QgnxMTw4uD7efA1gOXfbeDMcPfqblBe7W8',
-);
+import { AxiosInterceptors } from './axios';
+
 ReactDOM.render(
   <React.StrictMode>
     <AppContextProvider>
       <App />
+      <AxiosInterceptors />
     </AppContextProvider>
   </React.StrictMode>,
   document.getElementById('root'),
