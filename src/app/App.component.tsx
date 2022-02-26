85be9cda6c99df7dac567a7a088436cf97a98e99
Pavel Uvarov <pavel_uvarov@epam.com>
2022-02-26 16:04:29 +0300

    add task management page

--- src/app/App.component.tsx
+++ src/app/App.component.tsx
@@ -7,8 +7,11 @@ import { AppRouter, Profile, SideMenu } from './components';
 import { AppHeader } from './App.styles';
 import { useLocalStorage } from 'usehooks-ts';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const SIDER_COLLAPSED_LS_KEY = 'sider-collapsed';
+
+const queryClient = new QueryClient();
 
 export const App: FC = () => {
   const [isSiderCollapsed, setIsSiderCollapsed] = useLocalStorage(
@@ -17,24 +20,26 @@ export const App: FC = () => {
   );
 
   return (
-    <BrowserRouter>
-      <Layout className="app">
-        <Sider
-          collapsible
-          collapsed={isSiderCollapsed}
-          onCollapse={setIsSiderCollapsed}
-        >
-          <SideMenu />
-        </Sider>
-        <Layout>
-          <AppHeader>
-            <Profile />
-          </AppHeader>
-          <Content className="app-content">
-            <AppRouter />
-          </Content>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Layout className="app">
+          <Sider
+            collapsible
+            collapsed={isSiderCollapsed}
+            onCollapse={setIsSiderCollapsed}
+          >
+            <SideMenu />
+          </Sider>
+          <Layout>
+            <AppHeader>
+              <Profile />
+            </AppHeader>
+            <Content className="app-content">
+              <AppRouter />
+            </Content>
+          </Layout>
         </Layout>
-      </Layout>
-    </BrowserRouter>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 };
---
+++ src/pages/taskManagement/components/TaskCard.tsx
@@ -0,0 +1,82 @@
+import { Button, Card, DatePicker, Popconfirm, Spin, Typography } from 'antd';
+import moment from 'moment';
+import React, { FC, useEffect, useState } from 'react';
+import { Task } from '../../../types';
+import { axios } from '../../../axios';
+import { useMutation } from 'react-query';
+
+export const TaskCard: FC<{ task: Task; refetchList(): void }> = ({
+  task: taskRes,
+  refetchList,
+}) => {
+  const [task, setTask] = useState(taskRes);
+
+  const { isLoading, mutate } = useMutation<
+    Partial<Task>,
+    unknown,
+    Partial<Task>
+  >(taskPayload => {
+    return axios.put(`/tasks/${taskPayload._id}`, taskPayload);
+  });
+
+  useEffect(() => {
+    if (taskRes) {
+      setTask(taskRes);
+    }
+  }, [taskRes]);
+
+  const onValueChange = (partOfTask: Partial<Task>) => {
+    mutate({ ...partOfTask, _id: task._id });
+    setTask(p => ({ ...p, ...partOfTask }));
+  };
+
+  return (
+    <Spin spinning={isLoading}>
+      <Card
+        title={
+          <Typography.Paragraph
+            editable={{
+              onChange: name => onValueChange({ name }),
+            }}
+          >
+            {task.name}
+          </Typography.Paragraph>
+        }
+        extra={
+          <Popconfirm
+            title="Are you sure to delete this task?"
+            onConfirm={async () => {
+              await axios.delete(`/tasks/${task._id}`);
+              refetchList();
+            }}
+            okText="Yes"
+            cancelText="No"
+          >
+            <Button type="primary" danger ghost onClick={() => {}}>
+              Delete
+            </Button>
+          </Popconfirm>
+        }
+      >
+        <Typography.Paragraph
+          editable={{
+            onChange: description => onValueChange({ description }),
+          }}
+        >
+          {task.description}
+        </Typography.Paragraph>
+        <p>
+          Deadline:{' '}
+          <DatePicker
+            onChange={deadline =>
+              onValueChange({
+                deadline: moment(deadline).format(),
+              })
+            }
+            value={moment(task.deadline)}
+          />
+        </p>
+      </Card>
+    </Spin>
+  );
+};
