5e50d65f0e890ff57ace757b214d71ff96aa29c7
Sergey Zenin <sergey_zenin@epam.com>
2022-03-02 10:57:40 +0300

    remove stubs for calendar

--- src/pages/CalendarPage/CalendarPage.component.tsx
+++ src/pages/CalendarPage/CalendarPage.component.tsx
@@ -1,9 +1,9 @@
 import React, { FC, useCallback, useMemo } from 'react';
 import { Calendar, CalendarProps, Tag, Tooltip } from 'antd';
 import {
-  BookOutlined,
+  // BookOutlined,
   ExclamationCircleOutlined,
-  Html5Outlined,
+  // Html5Outlined,
 } from '@ant-design/icons';
 import moment, { Moment } from 'moment';
 import { useStudentScoreQuery } from '../../utils';
@@ -25,41 +25,37 @@ export const CalendarPage: FC = () => {
       })) || [];
 
     // place converted data of lectures and practice to add it to calendar data
-    const lecturesData: CalendarData[number][number][number] = scores.map(
-      ({ value }) => ({
-        type: 'lecture',
-        value: { ...value, name: 'test_lecture' },
-      }),
-    );
+    // const lecturesData: CalendarData[number][number][number] = scores.map(
+    //   ({ value }) => ({
+    //     type: 'lecture',
+    //     value: { ...value, name: 'test_lecture' },
+    //   }),
+    // );
+    //
+    // const practiceData: CalendarData[number][number][number] = scores.map(
+    //   ({ value }) => ({
+    //     type: 'practice',
+    //     value: { ...value, name: 'test_practice' },
+    //   }),
+    // );
 
-    const practiceData: CalendarData[number][number][number] = scores.map(
-      ({ value }) => ({
-        type: 'practice',
-        value: { ...value, name: 'test_practice' },
-      }),
-    );
-
-    return [...scores, ...practiceData, ...lecturesData]?.reduce<CalendarData>(
-      (result, { type, value }) => {
-        const year = value.deadlineDate.year();
-        const month = value.deadlineDate.month();
-        const date = value.deadlineDate.date();
-        return {
-          ...result,
-          [year]: {
-            ...(result[year] || {}),
-            [month]: {
-              ...(result[year]?.[month] || {}),
-              [date]: [
-                ...(result[year]?.[month]?.[date] || []),
-                { type, value },
-              ],
-            },
+    return [
+      ...scores /*, ...practiceData, ...lecturesData*/,
+    ]?.reduce<CalendarData>((result, { type, value }) => {
+      const year = value.deadlineDate.year();
+      const month = value.deadlineDate.month();
+      const date = value.deadlineDate.date();
+      return {
+        ...result,
+        [year]: {
+          ...(result[year] || {}),
+          [month]: {
+            ...(result[year]?.[month] || {}),
+            [date]: [...(result[year]?.[month]?.[date] || []), { type, value }],
           },
-        };
-      },
-      {} as CalendarData,
-    );
+        },
+      };
+    }, {} as CalendarData);
   }, [scoresData]);
 
   const dateCellRender: CalendarProps<Moment>['dateCellRender'] = useCallback(
@@ -78,23 +74,23 @@ export const CalendarPage: FC = () => {
               </Tooltip>
             );
 
-          case 'lecture':
-            return (
-              <Tooltip title={`Lecture: ${(data?.value as any)?.name}`}>
-                <Tag color="processing" icon={<BookOutlined />}>
-                  {(data?.value as any)?.name}
-                </Tag>
-              </Tooltip>
-            );
-
-          case 'practice':
-            return (
-              <Tooltip title={`Practice: ${(data?.value as any)?.name}`}>
-                <Tag color="success" icon={<Html5Outlined />}>
-                  {(data?.value as any)?.name}
-                </Tag>
-              </Tooltip>
-            );
+          // case 'lecture':
+          //   return (
+          //     <Tooltip title={`Lecture: ${(data?.value as any)?.name}`}>
+          //       <Tag color="processing" icon={<BookOutlined />}>
+          //         {(data?.value as any)?.name}
+          //       </Tag>
+          //     </Tooltip>
+          //   );
+          //
+          // case 'practice':
+          //   return (
+          //     <Tooltip title={`Practice: ${(data?.value as any)?.name}`}>
+          //       <Tag color="success" icon={<Html5Outlined />}>
+          //         {(data?.value as any)?.name}
+          //       </Tag>
+          //     </Tooltip>
+          //   );
 
           default:
             return null;
