a836c663666a613bcfbd7379c67d67648785ba03
Pavel Uvarov <pavel_uvarov@epam.com>
2022-03-09 19:05:15 +0300

    add filter by student

--- src/pages/dashboard/components/DashboardTaskCard.tsx
+++ src/pages/dashboard/components/DashboardTaskCard.tsx
@@ -75,181 +75,190 @@ export const DashboardTaskCard: FC<{
   };
 
   return (
-    <>
-      <Spin spinning={isLoading || isSendToRevisionLoading}>
-        <Badge.Ribbon
-          text={getStatusLabel(status)}
-          color={getStatusColor(status)}
-        >
-          <Card
-            title={
-              pullRequestLink ? (
-                <a href={pullRequestLink} target="_blank" rel="noreferrer">
-                  {task?.name}
-                </a>
-              ) : (
-                task?.name
-              )
-            }
-            extra={
-              <Flex alignItems="center" mt={16}>
-                {taskScore && (
-                  <Tooltip title={comment} placement="top">
-                    <Box mr={24}>
-                      <Space size="middle">
-                        <Rate allowHalf value={taskScore} disabled />
-                        {comment && <CommentOutlined color="green" />}
-                      </Space>
-                    </Box>
-                  </Tooltip>
-                )}
-                <Flex alignItems="center" mr={24}>
-                  <Space>
-                    <Avatar>{getName(student)?.slice(0, 1)}</Avatar>
-                    <Link to={`/profile/${student.login}`}>
-                      {getName(student)}
-                    </Link>
-                  </Space>
-                </Flex>
-                {task.taskLink && (
-                  <Tooltip title="Link to task description" placement="top">
-                    <Box mr={12}>
-                      <a href={task.taskLink} target="_blank" rel="noreferrer">
-                        <SelectOutlined />
-                      </a>
-                    </Box>
-                  </Tooltip>
-                )}
-              </Flex>
-            }
+    score && (
+      <>
+        <Spin spinning={isLoading || isSendToRevisionLoading}>
+          <Badge.Ribbon
+            text={getStatusLabel(status)}
+            color={getStatusColor(status)}
           >
-            <p>{task?.description}</p>
-            {score?.submissionDate && (
-              <p>
-                <Badge
-                  status="success"
-                  text={`Submitted on: ${moment(score?.submissionDate).format(
-                    'DD MMMM yyyy HH:mm',
-                  )}`}
-                />
-              </p>
-            )}
-            <p>
-              <Badge
-                status="error"
-                text={`Deadline: ${moment(task?.deadline).format(
-                  'DD MMMM yyyy',
-                )}`}
-              />
-            </p>
-            {score?.deadlineDate !== task?.deadline && (
-              <p>
-                <Tooltip title={score.deadlineChangeComment} placement="right">
+            <Card
+              title={
+                pullRequestLink ? (
+                  <a href={pullRequestLink} target="_blank" rel="noreferrer">
+                    {task?.name}
+                  </a>
+                ) : (
+                  task?.name
+                )
+              }
+              extra={
+                <Flex alignItems="center" mt={16}>
+                  {taskScore && (
+                    <Tooltip title={comment} placement="top">
+                      <Box mr={24}>
+                        <Space size="middle">
+                          <Rate allowHalf value={taskScore} disabled />
+                          {comment && <CommentOutlined color="green" />}
+                        </Space>
+                      </Box>
+                    </Tooltip>
+                  )}
+                  <Flex alignItems="center" mr={24}>
+                    <Space>
+                      <Avatar>{getName(student)?.slice(0, 1)}</Avatar>
+                      <Link to={`/profile/${student?.login}`}>
+                        {getName(student)}
+                      </Link>
+                    </Space>
+                  </Flex>
+                  {task.taskLink && (
+                    <Tooltip title="Link to task description" placement="top">
+                      <Box mr={12}>
+                        <a
+                          href={task.taskLink}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          <SelectOutlined />
+                        </a>
+                      </Box>
+                    </Tooltip>
+                  )}
+                </Flex>
+              }
+            >
+              <p>{task?.description}</p>
+              {score?.submissionDate && (
+                <p>
                   <Badge
-                    status="error"
-                    text={`Deadline changed to: ${moment(
-                      score?.deadlineDate,
-                    ).format('DD MMMM yyyy')}`}
+                    status="success"
+                    text={`Submitted on: ${moment(score?.submissionDate).format(
+                      'DD MMMM yyyy HH:mm',
+                    )}`}
                   />
-                </Tooltip>
-              </p>
-            )}
-
-            {score?.sendingForRevisionDate && (
+                </p>
+              )}
               <p>
                 <Badge
-                  status="processing"
-                  text={`Sent on revision: ${moment(
-                    score?.sendingForRevisionDate,
-                  ).format('DD MMMM yyyy HH:mm')}`}
-                />
-              </p>
-            )}
-            {score?.revisionDoneDate && (
-              <p>
-                <Badge
-                  status="success"
-                  text={`Revision done on: ${moment(
-                    score?.revisionDoneDate,
-                  ).format('DD MMMM yyyy HH:mm')}`}
-                />
-              </p>
-            )}
-            {score?.completionDate && (
-              <p>
-                <Badge
-                  status="success"
-                  text={`Completed on: ${moment(score?.completionDate).format(
-                    'DD MMMM yyyy HH:mm',
+                  status="error"
+                  text={`Deadline: ${moment(task?.deadline).format(
+                    'DD MMMM yyyy',
                   )}`}
                 />
               </p>
-            )}
-            <Box height={24} />
-
-            {pullRequestLink && (
-              <Typography.Paragraph
-                editable={{
-                  onChange: link => onUpdateLink(link),
-                }}
-              >
-                {link}
-              </Typography.Paragraph>
-            )}
-            <Space>
-              {(status === 'revisionDone' || status === 'onReview') && (
-                <Button
-                  type="primary"
-                  onClick={() => setIsCompleteModalVisible(true)}
-                >
-                  Complete
-                </Button>
-              )}
-              {status === 'onReview' && (
-                <>
-                  <Popconfirm
-                    title="Are sure you want to send to revision?"
-                    onConfirm={() => {
-                      sendToRevision(undefined, {
-                        onSuccess: () => {
-                          message.success('Task has been send to revision');
-                          refetchList();
-                        },
-                      });
-                    }}
-                    okText="Yes"
-                    cancelText="No"
+              {score?.deadlineDate !== task?.deadline && (
+                <p>
+                  <Tooltip
+                    title={score.deadlineChangeComment}
+                    placement="right"
                   >
-                    <Button type="primary" danger>
-                      Send to revision
-                    </Button>
-                  </Popconfirm>
-                </>
+                    <Badge
+                      status="error"
+                      text={`Deadline changed to: ${moment(
+                        score?.deadlineDate,
+                      ).format('DD MMMM yyyy')}`}
+                    />
+                  </Tooltip>
+                </p>
               )}
-              {status !== 'done' && (
-                <Button
-                  type="default"
-                  onClick={() => setIsUpdateDeadlineModalVisible(true)}
+
+              {score?.sendingForRevisionDate && (
+                <p>
+                  <Badge
+                    status="processing"
+                    text={`Sent on revision: ${moment(
+                      score?.sendingForRevisionDate,
+                    ).format('DD MMMM yyyy HH:mm')}`}
+                  />
+                </p>
+              )}
+              {score?.revisionDoneDate && (
+                <p>
+                  <Badge
+                    status="success"
+                    text={`Revision done on: ${moment(
+                      score?.revisionDoneDate,
+                    ).format('DD MMMM yyyy HH:mm')}`}
+                  />
+                </p>
+              )}
+              {score?.completionDate && (
+                <p>
+                  <Badge
+                    status="success"
+                    text={`Completed on: ${moment(score?.completionDate).format(
+                      'DD MMMM yyyy HH:mm',
+                    )}`}
+                  />
+                </p>
+              )}
+              <Box height={24} />
+
+              {pullRequestLink && (
+                <Typography.Paragraph
+                  editable={{
+                    onChange: link => onUpdateLink(link),
+                  }}
                 >
-                  Update deadline
-                </Button>
+                  {link}
+                </Typography.Paragraph>
               )}
-            </Space>
-          </Card>
-        </Badge.Ribbon>
-      </Spin>
-      <CompleteTaskModal
-        score={score}
-        refetchList={refetchList}
-        isVisible={isCompleteModalVisible}
-        setIsVisible={setIsCompleteModalVisible}
-      />
-      <UpdateDeadlineModal
-        score={score}
-        refetchList={refetchList}
-        isVisible={isUpdateDeadlineModalVisible}
-        setIsVisible={setIsUpdateDeadlineModalVisible}
-      />
-    </>
+              <Space>
+                {(status === 'revisionDone' || status === 'onReview') && (
+                  <Button
+                    type="primary"
+                    onClick={() => setIsCompleteModalVisible(true)}
+                  >
+                    Complete
+                  </Button>
+                )}
+                {status === 'onReview' && (
+                  <>
+                    <Popconfirm
+                      title="Are sure you want to send to revision?"
+                      onConfirm={() => {
+                        sendToRevision(undefined, {
+                          onSuccess: () => {
+                            message.success('Task has been send to revision');
+                            refetchList();
+                          },
+                        });
+                      }}
+                      okText="Yes"
+                      cancelText="No"
+                    >
+                      <Button type="primary" danger>
+                        Send to revision
+                      </Button>
+                    </Popconfirm>
+                  </>
+                )}
+                {status !== 'done' && (
+                  <Button
+                    type="default"
+                    onClick={() => setIsUpdateDeadlineModalVisible(true)}
+                  >
+                    Update deadline
+                  </Button>
+                )}
+              </Space>
+            </Card>
+          </Badge.Ribbon>
+        </Spin>
+        <CompleteTaskModal
+          score={score}
+          refetchList={refetchList}
+          isVisible={isCompleteModalVisible}
+          setIsVisible={setIsCompleteModalVisible}
+        />
+        <UpdateDeadlineModal
+          score={score}
+          refetchList={refetchList}
+          isVisible={isUpdateDeadlineModalVisible}
+          setIsVisible={setIsUpdateDeadlineModalVisible}
+        />
+      </>
+    )
   );
 };
